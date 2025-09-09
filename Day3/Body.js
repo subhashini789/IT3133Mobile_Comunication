482f9b71bca22f106ee74f25ffd6a2fc049d3724
subhashini789 <sandunisubashini22@gmail.com>
2025-09-09 10:31:43 -0700

    Add files via upload

---
+++ Day3/Body.js
@@ -0,0 +1,22 @@
+import SearchBar from './Searchbar';
+function Body(){
+    return(
+        <>
+        <div style={{padding:"20px"}}>
+                <SearchBar/>
+            </div>
+        <div style={{
+            padding:"500px",
+           backgroundColor:"rgba(44, 145, 28, 0.6)",
+           display:"flex"
+        }}>
+            
+              
+         <h1>Body</h1>
+        </div>
+        </>
+         
+    );
+}
+
+export default Body;
